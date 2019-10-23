b3cdf653579fc28879991430971bd3d595cbc77a
Alex Li <alex_bn_lee@hotmail.com>
2019-10-23 17:30:45 +1100

    Create 05_export_table_to_csv.py

---
+++ shapefile processing/05_export_table_to_csv.py
@@ -0,0 +1,3 @@
+# Tool: Export Feature Attribute to ASCII
+
+arcpy.ExportXYv_stats('Suburbs_MB_2016_AUS', 'SA2_NAME16;CENTROID_X;CENTROID_Y;State;Shape_Area', 'COMMA', r'D:\Twitter Data\Data\test\2.csv', 'ADD_FIELD_NAMES')
